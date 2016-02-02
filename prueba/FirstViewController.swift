6ba189f016dc91b1ff153cc5531a6b8cf7df4b5c
raul <raultc@macbook-pro-de-r.local>
2016-02-01 20:36:59 -0600

    Initial Commit

---
+++ prueba/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  prueba
+//
+//  Created by Raúl T on 01/02/16.
+//  Copyright © 2016 rtc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
