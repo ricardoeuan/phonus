84b60a34586368e934c15e324c27493a1a0ff012
raul <raultc@macbook-pro-de-r.local>
2016-02-09 18:02:50 -0600

    iOS - added test - v1

--- prueba/FirstViewController.swift
+++ prueba/FirstViewController.swift
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let edades = ["8","9","10","11","12","..."]
+    
+
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+    return 1
+    }
+
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return edades.count
+    }
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+    return edades[row]
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    
     }
-
+    
 
 }
 
