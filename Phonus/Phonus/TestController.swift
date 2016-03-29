5127b8d70edda26178ad016ef52ddbe923ecf259
raul <raultc@macbook-pro-de-r.local>
2016-03-29 15:34:10 -0600

    IOS - RATC - v1.2.1

--- Phonus/Phonus/TestController.swift
+++ Phonus/Phonus/TestController.swift
@@ -15,26 +15,49 @@ class Test: UIViewController {
     var minus = true
     var plus = true
     
+    @IBOutlet var playB: UIButton!
+    @IBAction func playButton(sender: AnyObject) {
+        
+        playB.hidden = true
+        yesB.hidden = false
+        noB.hidden = false
+        //continueButton.hidden = false
+        
+    }
+    
     @IBOutlet var continueButton: UIButton!
     
     @IBOutlet weak var value: UILabel!
     
     @IBOutlet weak var slider: UISlider!
     
+    @IBOutlet var yesB: UIButton!
     @IBAction func yesButton(sender: AnyObject) {
         
-        slider.value += 1000
+        if plus == false {
+            
+            continueButton.hidden = false
+            noB.hidden = true
+            yesB.hidden = true
+            
+        }
+        
+        else {
+            slider.value += 1000
+        }
+        
+        
         hzValue()
+        plus = true
 
     }
     
+    @IBOutlet var noB: UIButton!
     @IBAction func noButton(sender: AnyObject) {
         
         slider.value -= 100
         hzValue()
-
-        //continueButton.hidden = false
-
+        plus = false
         
     }
     
@@ -47,8 +70,9 @@ class Test: UIViewController {
     
     override func viewDidLoad() {
         
-        //continueButton.hidden = true
-        
+        yesB.hidden = true
+        noB.hidden = true
+        continueButton.hidden = true
         value.text = String(Int(slider.value))
         
         
